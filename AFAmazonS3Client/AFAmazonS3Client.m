2cd0a101c0337fd67f09eebf2fa3bbcc87b965df
Alexander Edge <alex@alexedge.co.uk>
2013-02-13 15:38:32 +0000

    Add calculation of AWS signature

--- AFAmazonS3Client/AFAmazonS3Client.m
+++ AFAmazonS3Client/AFAmazonS3Client.m
@@ -23,9 +23,73 @@
 
 #import "AFAmazonS3Client.h"
 #import "AFXMLRequestOperation.h"
+#import "NSData+SHA1.h"
+#import <CommonCrypto/CommonHMAC.h>
+#import "ISO8601DateFormatter.h"
+
 
 NSString * const kAFAmazonS3BaseURLString = @"https://s3.amazonaws.com";
 NSString * const kAFAmazonS3BucketBaseURLFormatString = @"https://%@.s3.amazonaws.com";
+
+static NSString * AFPercentEscapedQueryStringPairMemberFromStringWithEncoding(NSString *string, NSStringEncoding encoding) {
+    static NSString * const kAFCharactersToBeEscaped = @":/?&=;+!@#$()~'";
+    static NSString * const kAFCharactersToLeaveUnescaped = @"[].";
+    
+	return (__bridge_transfer  NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (__bridge CFStringRef)string, (__bridge CFStringRef)kAFCharactersToLeaveUnescaped, (__bridge CFStringRef)kAFCharactersToBeEscaped, CFStringConvertNSStringEncodingToEncoding(encoding));
+}
+
+static NSString * AFBase64EncodedStringFromData(NSData *data);
+
+static NSString * AFBase64EncodedStringFromString(NSString *string) {
+    NSData *data = [NSData dataWithBytes:[string UTF8String] length:[string lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+    return AFBase64EncodedStringFromData(data);
+}
+
+static NSString * AFBase64EncodedStringFromData(NSData *data) {
+    
+    NSUInteger length = [data length];
+    NSMutableData *mutableData = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+    
+    uint8_t *input = (uint8_t *)[data bytes];
+    uint8_t *output = (uint8_t *)[mutableData mutableBytes];
+    
+    for (NSUInteger i = 0; i < length; i += 3) {
+        NSUInteger value = 0;
+        for (NSUInteger j = i; j < (i + 3); j++) {
+            value <<= 8;
+            if (j < length) {
+                value |= (0xFF & input[j]);
+            }
+        }
+        
+        static uint8_t const kAFBase64EncodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        
+        NSUInteger idx = (i / 3) * 4;
+        output[idx + 0] = kAFBase64EncodingTable[(value >> 18) & 0x3F];
+        output[idx + 1] = kAFBase64EncodingTable[(value >> 12) & 0x3F];
+        output[idx + 2] = (i + 1) < length ? kAFBase64EncodingTable[(value >> 6)  & 0x3F] : '=';
+        output[idx + 3] = (i + 2) < length ? kAFBase64EncodingTable[(value >> 0)  & 0x3F] : '=';
+    }
+    
+    return [[NSString alloc] initWithData:mutableData encoding:NSASCIIStringEncoding];
+    
+}
+
+static NSData * AFHMACSHA1FromStringWithKey(NSString *string, NSString *key){
+    
+    NSData *clearTextData = [string dataUsingEncoding:NSUTF8StringEncoding];
+	NSData *keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
+    
+	uint8_t digest[CC_SHA1_DIGEST_LENGTH] = {0};
+    
+	CCHmacContext hmacContext;
+	CCHmacInit(&hmacContext, kCCHmacAlgSHA1, keyData.bytes, keyData.length);
+	CCHmacUpdate(&hmacContext, clearTextData.bytes, clearTextData.length);
+	CCHmacFinal(&hmacContext, digest);
+    
+	return [NSData dataWithBytes:digest length:CC_SHA1_DIGEST_LENGTH];
+    
+}
 
 #pragma mark -
 
@@ -241,10 +305,18 @@ NSString * const kAFAmazonS3BucketBaseURLFormatString = @"https://%@.s3.amazonaw
 
     if (data && response) {
         NSMutableURLRequest *request = [self multipartFormRequestWithMethod:method path:destinationPath parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+            
+            NSString *policyDocument = [self policyDocumentForMIMEtype:[response MIMEType] filename:[filePath lastPathComponent]];
+            
+            [formData appendPartWithFormData:[self.accessKey dataUsingEncoding:NSUTF8StringEncoding] name:@"AWSAccessKeyId"];
+            [formData appendPartWithFormData:[policyDocument dataUsingEncoding:NSUTF8StringEncoding] name:@"Policy"];
+            [formData appendPartWithFormData:[AFBase64EncodedStringFromData(AFHMACSHA1FromStringWithKey(policyDocument, self.secret)) dataUsingEncoding:NSUTF8StringEncoding] name:@"Signature"];
             [formData appendPartWithFormData:[[filePath lastPathComponent] dataUsingEncoding:NSUTF8StringEncoding] name:@"key"];
             [formData appendPartWithFileData:data name:@"file" fileName:[filePath lastPathComponent] mimeType:[response MIMEType]];
+            
         }];
-
+        
+        
         AFHTTPRequestOperation *requestOperation = [self HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
             if (success) {
                 success(responseObject);
@@ -258,7 +330,49 @@ NSString * const kAFAmazonS3BucketBaseURLFormatString = @"https://%@.s3.amazonaw
         [requestOperation setUploadProgressBlock:progress];
 
         [self enqueueHTTPRequestOperation:requestOperation];
+        
     }
 }
+
+- (ISO8601DateFormatter *)dateFormatter{
+    
+    static ISO8601DateFormatter *_dateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _dateFormatter = [[ISO8601DateFormatter alloc] init];
+        _dateFormatter.includeTime = YES;
+    });
+    
+    return _dateFormatter;
+    
+}
+
+- (NSString *)policyDocumentForMIMEtype:(NSString *)mimeType filename:(NSString *)filename{
+    
+    NSMutableDictionary *policy = [NSMutableDictionary dictionary];
+    
+    NSDate *expirationDate = [NSDate dateWithTimeIntervalSinceNow:60*60];
+    
+    NSString *expirationDateString = [[self dateFormatter] stringFromDate:expirationDate timeZone:[NSTimeZone defaultTimeZone]];
+    
+    policy[@"expiration"] = expirationDateString;
+    
+    NSArray *conditions = @[@{@"bucket" : self.bucket},@[@"starts-with",@"$key",filename]];
+    
+    policy[@"conditions"] = conditions;
+    
+    NSError *error = nil;
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:policy options:0 error:&error];
+    
+    if (error) {
+        return nil;
+    }
+    
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    return AFBase64EncodedStringFromString(jsonString);
+}
+
 
 @end
