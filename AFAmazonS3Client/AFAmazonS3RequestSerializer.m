2ac7ae52910fb2104a0c91b6f647166c7f0a833f
Eli Perkins <eli@onemightyroar.com>
2014-03-19 11:27:42 -0400

    Fixing whitespace

--- AFAmazonS3Client/AFAmazonS3RequestSerializer.m
+++ AFAmazonS3Client/AFAmazonS3RequestSerializer.m
@@ -158,8 +158,8 @@ static NSString * AFBase64EncodedStringFromData(NSData *data) {
 			[mutableCanonicalizedAMZHeaderString appendFormat:@"%@:%@\n", key, value];
 		}
 
-        NSString *canonicalizedResource = [NSString stringWithFormat:@"/%@%@", self.bucket, request.URL.path];
-    	NSString *method = [request HTTPMethod];
+    NSString *canonicalizedResource = [NSString stringWithFormat:@"/%@%@", self.bucket, request.URL.path];
+    NSString *method = [request HTTPMethod];
 		NSString *contentMD5 = [request valueForHTTPHeaderField:@"Content-MD5"];
 		NSString *contentType = [request valueForHTTPHeaderField:@"Content-Type"];
 		NSString *date = AFRFC822FormatStringFromDate([NSDate date]);
