caea723aadddfb24fe35754f6190015e815e1510
Mattt Thompson <m@mattt.me>
2014-11-29 10:51:53 -0800

    Using dot syntax

--- AFAmazonS3Client/AFAmazonS3Manager.m
+++ AFAmazonS3Client/AFAmazonS3Manager.m
@@ -220,7 +220,7 @@ NSString * const AFAmazonS3ManagerErrorDomain = @"com.alamofire.networking.s3.er
                     failure:(void (^)(NSError *error))failure
 {
     NSMutableURLRequest *fileRequest = [NSMutableURLRequest requestWithURL:[NSURL fileURLWithPath:filePath]];
-    [fileRequest setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    fileRequest.cachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
 
     NSURLResponse *response = nil;
     NSError *fileError = nil;
